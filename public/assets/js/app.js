ac0f829fc24ddca6d31e094945e02537c39d355c
Miriam <miriammp1997@gmail.com>
2017-07-20 15:52:01 -0500

    add part of firebase

---
+++ public/assets/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCcxON3o7U44BBuen1VzVn8MQicwtbf1qA",
+  authDomain: "mony-test.firebaseapp.com",
+  databaseURL: "https://mony-test.firebaseio.com",
+  projectId: "mony-test",
+  storageBucket: "mony-test.appspot.com",
+  messagingSenderId: "947896289488"
+};
+firebase.initializeApp(config);
